0dd2cadd739d99a3a21a2a074ebd9fa464525147
Damon McMinn <damonrmcminn@gmail.com>
2015-04-28 11:10:23 +0100

    Store user data

--- src/login/factory.js
+++ src/login/factory.js
@@ -6,6 +6,7 @@ export default function(API, ls, pubsub) {
       return API.login(user)
         .success(res => {
           ls.set('token', res.token);
+          ls.set('user', res.data);
           pubsub.pub('login', true);
           return null;
         });
