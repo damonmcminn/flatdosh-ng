948b97a6e76a7e71f9847ac033a5d20664f96f5d
Damon McMinn <damonrmcminn@gmail.com>
2015-04-28 11:39:04 +0100

    conditional source maps

--- gulp/es6/tasks/javascript.js
+++ gulp/es6/tasks/javascript.js
@@ -18,7 +18,7 @@ export default function() {
   .bundle()
   .pipe(source('bundle.js'))
   .pipe(buffer())
-  .pipe(sourcemaps.init({loadMaps: true}))
+  .pipe(sourcemaps.init({loadMaps: process.env.NODE_ENV === 'dev'}))
   // transform tasks follow
   .pipe(preprocess())
   .pipe(uglify())
