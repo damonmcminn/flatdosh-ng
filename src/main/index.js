afb2aa183997f2054d771a09b646ad2fa3484482
Damon McMinn <damonrmcminn@gmail.com>
2015-04-28 11:11:26 +0100

    Show username; 'routing'...; logout; navbar collapsing

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,19 @@
 export default function(ls, pubsub) {
+
   let main = this;
 
-  // clear token...
-  pubsub.sub('logout', () => main.loggedIn = false);
+  let user = ls.get('user');
+  let {name} = user;
+
+  main.name = name;
+
+  main.change = function(page) {
+    main.toggleCollapsed();
+    main.page = page;
+  }
+
+  pubsub.sub('page', page => main.page = page);
+
   pubsub.sub('login', success => {
     if (success) {
       main.loggedIn = true;
@@ -10,5 +21,17 @@ export default function(ls, pubsub) {
   });
 
   main.loggedIn = !!ls.get('token');
+
+  main.logout = function() {
+
+    ls.drop('token');
+    main.loggedIn = false;
+
+  }
+
+  main.collapsed = true;
+  main.toggleCollapsed = function() {
+    main.collapsed = !main.collapsed;
+  }
 
 }
