90c3995f99e70a16b00c0aee05e5dca52f075ca1
Damon McMinn <damonrmcminn@gmail.com>
2015-04-29 11:25:31 +0100

    Conditional login/register

--- src/login/ctrl.js
+++ src/login/ctrl.js
@@ -1,23 +1,42 @@
-export default function(loginFactory) {
+export default function(login, register) {
 
   let vm = this;
-  let login = loginFactory;
 
-  vm.login = function() {
-    login.do(vm.user).error(failure => {
-
-      let message = failure.message;
-
-      vm.error = message;
-
-      switch (message) {
-        case 'Bad password':
-          vm.user.password = null;
-          break;
-        case 'User not found':
-          vm.user = null;
-      }
-    });
+  vm.select = function(type) {
+    vm.action = type;
   }
 
+  vm.selectLogin = true;
+
+  vm.submit = function() {
+    actions[vm.action]();
+  }
+
+  let actions = {
+    
+    login () {
+      login.do(vm.user).error(failure => {
+
+        let message = failure.message;
+
+        vm.error = message;
+
+        switch (message) {
+          case 'Bad password':
+            vm.user.password = null;
+            break;
+          case 'User not found':
+            vm.user = null;
+        }
+      });
+
+    },
+    register () {
+      register.do(vm.user)
+        .success(r => {
+          actions.login();
+        })
+        .error(err => console.log(err));
+    }
+  };
 }
