fcd9e415d86df6ed0f73cccb3d26c2a52af2152b
Damon McMinn <damonrmcminn@gmail.com>
2015-04-30 15:43:38 +0100

    ESLint fixes; show error message

--- src/login/ctrl.js
+++ src/login/ctrl.js
@@ -2,22 +2,19 @@ export default function(login, register) {
 
   let vm = this;
 
-  vm.select = function(type) {
-    vm.action = type;
-  }
-
   vm.selectLogin = true;
 
-  vm.submit = function() {
-    actions[vm.action]();
-  }
-
   let actions = {
-    
+
     login () {
       login.do(vm.user).error(failure => {
 
-        let message = failure.message;
+        let message;
+        if (!failure || !failure.message) {
+          message = 'Server error';
+        } else {
+          message = failure.message;
+        }
 
         vm.error = message;
 
@@ -27,16 +24,26 @@ export default function(login, register) {
             break;
           case 'User not found':
             vm.user = null;
+            break;
         }
       });
 
     },
     register () {
       register.do(vm.user)
-        .success(r => {
+        .success(() => {
           actions.login();
         })
         .error(err => console.log(err));
     }
   };
+
+  vm.select = function(type) {
+    vm.action = type;
+  };
+
+  vm.submit = function() {
+    actions[vm.action]();
+  };
+
 }
