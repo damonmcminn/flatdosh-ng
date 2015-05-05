ee6531fb4ea6bf1dd990adf7fd0dc1138a2529d7
Damon McMinn <damonrmcminn@gmail.com>
2015-05-05 12:22:11 +0100

    Add group factory; amend DI

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import auth from './auth';
 import balance from './balance';
 import expense from './expense';
 import {money} from './filters';
+import group from './group';
 import history from './history';
 import login from './login';
 import ls from './ls';
@@ -13,21 +14,22 @@ import user from './user';
 
 angular.module('flatdosh', ['templates', 'ui.bootstrap'])
   // FACTORIES
-  .factory('API', ['$http', '$window', API])
+  .factory('API', ['$http', '$window', 'group', API])
   .factory('auth', ['ls', auth])
   .factory('balance', ['API', balance.factory])
   .factory('expense', ['API', 'pubsub', expense.factory])
-  .factory('login', ['API', 'ls', 'pubsub', login.factory])
+  .factory('group', ['ls', group.factory])
+  .factory('login', ['API', 'ls', 'pubsub', 'group', login.factory])
   .factory('ls', [ls])
   .factory('pubsub', [pubsub])
   .factory('register', ['API', register.factory])
   .factory('user', [user.factory])
   // CONTROLLERS
   .controller('balanceCtrl', ['balance', 'pubsub', balance.ctrl])
-  .controller('expenseCtrl', ['expense', 'pubsub', expense.ctrl])
+  .controller('expenseCtrl', ['expense', 'pubsub', 'group', expense.ctrl])
   .controller('loginCtrl', ['login', 'register', login.ctrl])
   .controller('historyCtrl', ['expense', 'pubsub', 'ls', history.ctrl])
-  .controller('mainCtrl', ['ls', 'pubsub', main])
+  .controller('mainCtrl', ['ls', 'pubsub', 'group', 'API', main])
   .controller('userCtrl', ['user', user.ctrl])
   //CONFIG
   .config(['$httpProvider', hp => {
