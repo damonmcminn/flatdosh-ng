17a4d1a2c48f12edec564c59023789686e72be19
Damon McMinn <damonrmcminn@gmail.com>
2015-05-06 18:36:58 +0100

    Refactor: clean up shit

--- src/balance/factory.js
+++ src/balance/factory.js
@@ -7,29 +7,7 @@ export default function(API) {
       return API.balance()
         .success(balances => {
 
-          // attach labels for determing warning/danger classes
-
-          // the user with the largest negative balance
-          let debt = balances.reduce((prev, curr) => {
-            return prev.balance < curr.balance ? prev : curr;
-          });
-
-          // flag debt for styling
-          if (debt.balance < 0) {
-            debt.danger = true;
-          }
-
-          // only executes if balances are less than zero
-          // ergo a debt.danger exists
-          balances.filter(balance => (!balance.danger && balance.balance < 0))
-            .forEach(balance => {
-              if (balance.balance === debt.balance) {
-                balance.danger = true;
-              } else {
-                balance.warning = true;
-              }
-            });
-
+          // stupid email addresses..
           balances.forEach(balance => {
             balance.name = balance.name.split('@').shift();
           });
