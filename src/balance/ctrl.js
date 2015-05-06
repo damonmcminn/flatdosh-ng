408646c9dd02e759e71cd6d0a1d7903f4742daa2
Damon McMinn <damonrmcminn@gmail.com>
2015-05-06 18:36:23 +0100

    Log errs

--- src/balance/ctrl.js
+++ src/balance/ctrl.js
@@ -1,15 +1,13 @@
-export default function(balance, pubsub) {
+export default function(balance) {
 
   let vm = this;
 
   function getBalances() {
-    balance.get().success(balances => vm.balances = balances);
+    balance.get()
+      .success(balances => vm.balances = balances)
+      .error(err => console.log(err));
   }
 
   getBalances();
-
-  pubsub.sub('update balances', () => {
-    getBalances();
-  });
 
 }
