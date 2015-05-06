58883b257d1b09eb6dc1c9a885e7998c6f4cc166
Damon McMinn <damonrmcminn@gmail.com>
2015-05-06 18:33:16 +0100

    Group model

--- src/group/factory.js
+++ src/group/factory.js
@@ -1,5 +1,20 @@
 export default function(ls) {
 
-  return {};
+  let {groups} = ls.get('user');
+  let group = ls.get('group');
+
+  if (!group) {
+    let first = groups[0];
+    ls.set('group', first);
+    group = first;
+  }
+
+  let {id, name} = group;
+
+  return {
+    all: groups,
+    id,
+    name
+  };
 
 }
